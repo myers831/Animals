8c82df2966bb9539f74103b8a615511044586e41
Mac <myers831@yahoo.com>
2017-10-29 12:09:30 -0400

    done

--- app/src/main/java/com/example/admin/animals/FelineDatabaseHelper.java
+++ app/src/main/java/com/example/admin/animals/FelineDatabaseHelper.java
@@ -37,7 +37,7 @@ public class FelineDatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { 
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
